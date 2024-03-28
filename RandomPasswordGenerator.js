edb651c940433e3937f4254139ad74c7dc415a49
PrzemekO84 <przemekorzechowski53@gmail.com>
2024-03-28 14:34:55 +0100

    Added checkbox styling and started to working on shuffle method

--- RandomPasswordGenerator.js
+++ RandomPasswordGenerator.js
@@ -12,27 +12,42 @@ const createPassword = document.getElementById("createPassword");
 const result = document.getElementById("result");
 const saveToClipBoard = document.getElementById("saveToClipBoard");
 
-function generateRandomPassword(){
+//Shuffle function
 
-    const password = [];
-
-    const passwordLengthValue = passwordLength.value;
-    const passwordLengthNumber = Number(passwordLengthValue);
-
-    console.log(typeof passwordLengthNumber);
-
-    if(letter.checked){
-        password.push(randomLetters)
+function shuffle(array){
+    for(let i = array.length - 1; i > 0; i--){
+        const j = Math.floor(Math.random() * (i+1));
+        [array[i], array[j]] = [array[j], array[i]];
     }
-    if(numbers.checked){
-        password.push(randomNumbers)
-    }
-    if(characters.checked){
-        password.push(randomCharacters)
-    }
-
-    console.log(password.join(""));
+    return array;
 }
 
+function generateRandomPassword(){
+
+    const charactersArray = [];
+    const password = [];
+
+    const passwordLengthNumber = Number(passwordLength.value);
+
+    console.log(passwordLengthNumber);
+
+    if(letter.checked){
+        charactersArray.push(...randomLetters.split('')); // Split string into array of characters
+    }
+    if(numbers.checked){
+        charactersArray.push(...randomNumbers.split(''));
+    }
+    if(characters.checked){
+        charactersArray.push(...randomCharacters.split(''));
+    }
+
+    const joinedCharacters = charactersArray.join("");
+
+    console.log(joinedCharacters);
+
+    const shuffeldCharacters = shuffle(joinedCharacters.split("")).join("");
+
+    console.log(shuffeldCharacters);
+}
 
 createPassword.onclick = generateRandomPassword;
